10fb9a584cc4b303aa393d9be7b919359d216efb
Benjamin Dirix <BenjaminDirix@hotmail.com>
2018-10-23 15:27:21 +0200

    Taak 1.9 Regular expressions => regex.cs

---
+++ XMLFundamentalsTakenMateriaal/Regex.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Regex
+{
+    public Regex()
+    {
+        var email = new Regex(@".+@.+\..+");
+        var bankRekeningNummer = new Regex(@"[0-9]{3}-[0-9]{7}-[0-9]{2}");
+        var nummerplaat = new Regex(@"[0-9]-[A-Z]{3}-[0-9]{3}");
+    }
+}
