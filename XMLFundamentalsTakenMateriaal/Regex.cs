5f82f0a7b2ba00b5475b28143449d4ba6e4d7591
Benjamin Dirix <BenjaminDirix@hotmail.com>
2018-10-26 13:56:41 +0200

    26102018

--- XMLFundamentalsTakenMateriaal/Regex.cs
+++ XMLFundamentalsTakenMateriaal/Regex.cs
@@ -4,8 +4,8 @@ public class Regex
 {
     public Regex()
     {
-        var email = new Regex(@".+@.+\..+");
-        var bankRekeningNummer = new Regex(@"[0-9]{3}-[0-9]{7}-[0-9]{2}");
-        var nummerplaat = new Regex(@"[0-9]-[A-Z]{3}-[0-9]{3}");
+        var email = new Regex(@"^.+@.+\..+$");
+        var bankRekeningNummer = new Regex(@"^\d{3}-\d{7}-\d{2}$");
+        var nummerplaat = new Regex(@"^\d-[A-Z]{3}-\d{3}$");
     }
 }
